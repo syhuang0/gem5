59ce37f80a723ba7621fe9857232119d11b11ecb
shaoyu <shaoyuh16@gmail.com>
2020-12-04 16:02:16 -0500

    change configuration

--- src/mem/SimpleMemory.py
+++ src/mem/SimpleMemory.py
@@ -43,7 +43,7 @@ class SimpleMemory(AbstractMemory):
     type = 'SimpleMemory'
     cxx_header = "mem/simple_mem.hh"
     port = SlavePort("Slave ports")
-    latency = Param.Latency('30ns', "Request to response latency")
+    latency = Param.Latency('300ns', "Request to response latency")
     latency_var = Param.Latency('0ns', "Request to response latency variance")
     # The memory bandwidth limit default is set to 12.8GB/s which is
     # representative of a x64 DDR3-1600 channel.
