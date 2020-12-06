f425259f33082ed14de11421251838c546d797b1
Shao-Yu Huang <huan1464@qstruct.ecn.purdue.edu>
2020-12-06 15:55:46 -0500

    fix benchmark script

--- bench_mark.sh
+++ bench_mark.sh
@@ -9,7 +9,7 @@ write_thresh()
 
 run_bench()
 {
-	nice -n 10 ./build/ARM/gem5.opt -d "$2/$1" configs/spec2k6/run.py -b "$1" --cpu-type=DerivO3CPU --caches --l2cache --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=8 --l1d_size=32kB --l1i_size=32kB --l2_size=2MB --cpu-clock=1000000000 --fast-forward=500000 --warmup-insts=500000 --standard-switch=1 --maxinsts=1000000000
+	nice -n 10 ./build/ARM/gem5.opt -d "$2/$1" configs/spec2k6/run.py -b "$1" --cpu-type=DerivO3CPU --caches --l2cache --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=8 --l1d_size=32kB --l1i_size=32kB --l2_size=2MB --cpu-clock=1000000000 --warmup-insts=500000 --standard-switch=500000 --maxinsts=1000000000
 	
 }
 
@@ -24,7 +24,7 @@ for j in 0;
 do
 	#echo "BGDTHRESH=$j"
 	#write_thresh "$j"
-        scons -j 8 ./build/ARM/gem5.opt
+        #scons-3 -j 8 ./build/ARM/gem5.opt
 	sleep 1
 	for i in $bench_list;
 	do
