059302080c67d8cdb3d9af1a1579d1f92e95f7cd
shaoyu <shaoyuh16@gmail.com>
2020-12-06 14:54:33 -0500

    add benchmark script

---
+++ bench_mark.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+
+write_thresh()
+{
+	echo "#define BDGTHRESH $1" >src/cpu/minor/bgdthresh.hh
+}
+
+
+run_bench()
+{
+	nice -n 10 ./build/ARM/gem5.opt -d "$2/$1" configs/spec2k6/run.py -b "$1" --cpu-type=DerivO3CPU --caches --l2cache --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=8 --l1d_size=32kB --l1i_size=32kB --l2_size=2MB --cpu-clock=1000000000 --fast-forward=500000 --warmup-insts=500000 --standard-switch=1 --maxinsts=1000000000
+	
+}
+
+
+branch_name="$1"
+
+shift
+
+bench_list="$@"
+
+for j in 0;
+do
+	#echo "BGDTHRESH=$j"
+	#write_thresh "$j"
+        scons -j 8 ./build/ARM/gem5.opt
+	sleep 1
+	for i in $bench_list;
+	do
+    		echo "start $i"
+    		run_bench "$i" "$branch_name" &
+                process_id="process_id $!"
+	done
+        for i in $process_id;
+	do
+		wait "$i"
+	done
+done
